bd0ad45d049e052f8e6d50432ebaff34edb0f5de
Ivan Bakaidov <aacidov@Air-Ivan.Dlink>
2017-10-13 12:41:00 +0300

    Add controlls modules dir

---
+++ src/renderer/components/LandingPage/Controlls/Alarm.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="btn btn-default  ic-add-alert-24px" @click="alarm">
+        .
+    </button>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+            alarmPlayer: new Audio('/static/alarm.wav')
+        }
+    },
+    methods: {
+        alarm() {
+            this.alarmPlayer.pause();
+            this.alarmPlayer.currentTime = 0;
+            this.alarmPlayer.play();
+        }
+    },
+    mounted() {
+    this.$mousetrap.bind('mod+f', this.alarm);
+  }
+
+}
+</script>
+
