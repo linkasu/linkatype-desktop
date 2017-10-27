d4755572d26a762b7bc41e3d4a1793edb2e4b7f0
Ivan Bakaidov <aacidov@Air-Ivan.Dlink>
2017-10-27 21:33:24 +0300

    add tts stopper for online

--- src/renderer/node/say.js
+++ src/renderer/node/say.js
@@ -3,13 +3,15 @@ import yakit from './yandexspeechkit';
 
 import SettingsVue from '../components/LandingPage/Settings.vue'
 
-const yaapikey='a095c3ee-6695-4311-bae4-e875dca7e131';
+const yaapikey = 'a095c3ee-6695-4311-bae4-e875dca7e131';
 
 let settings = SettingsVue.data();
 
 yakit.call(window)
 
 let yatts = null;
+
+let speaking = false;
 
 export default {
     speak(text) {
@@ -18,22 +20,27 @@ export default {
             say.speak(text);
             return;
         }
-        if (yatts ==null){
+        if (yatts == null) {
             initya();
         }
-        yatts.speak(text, {
-            speaker: settings.settings.tts.voice  
-            
-        })
+        if (!speaking) {
+            speaking = true;
+            yatts.speak(text, {
+                speaker: settings.settings.tts.voice,
+                stopCallback() {
+                    speaking = false;
+                }
+            });
+        }
     }
 }
 
-function initya(){
+function initya() {
     yatts = window.ya.speechkit.Tts(
         // Настройки синтеза. Список доступных настроек см. в справочнике.
         {
-          // API-ключ. Может быть задан глобально через объект ya.speechkit.settings.
-          apikey: yaapikey
-    }
-      );
+            // API-ключ. Может быть задан глобально через объект ya.speechkit.settings.
+            apikey: yaapikey
+        }
+    );
 }
