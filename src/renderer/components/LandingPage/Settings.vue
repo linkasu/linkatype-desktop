53f30af15491342c15507031984cde17317fbe19
Ivan Bakaidov <aacidov@Air-Ivan.Dlink>
2017-10-27 20:51:57 +0300

    add online tts (yandex)

--- src/renderer/components/LandingPage/Settings.vue
+++ src/renderer/components/LandingPage/Settings.vue
@@ -9,6 +9,12 @@
     <input type="button" class="btn btn-default" id="typingSoundTumbler" :value="settings.common.typingSound?'Выключить':'Включить'" :class='{"btn-success":settings.common.typingSound}' @click='settings.common.typingSound=!settings.common.typingSound'>
 
   </div>
+
+  <div class="form-group">
+    <label for="onlineTtsTumbler">Онлайн озвучка текста:</label>
+    <input type="button" class="btn btn-default" id="onlineTtsTumbler" :value="settings.tts.offline?'Включить':'Выключить'" :class='{"btn-success":!settings.tts.offline}' @click='settings.tts.offline=!settings.tts.offline'>
+
+  </div>
     </form>
 </div>
 </template>
--- src/renderer/node/say.js
+++ src/renderer/node/say.js
@@ -1,11 +1,37 @@
 import say from 'say';
+import yakit from './yandexspeechkit';
+
+import SettingsVue from '../components/LandingPage/Settings.vue'
+
+const yaapikey='a095c3ee-6695-4311-bae4-e875dca7e131';
+
+let settings = SettingsVue.data();
+
+yakit.call(window)
+
+let yatts = null;
 
 export default {
-    settings:{
-
-    },
-    speak(text){
-        say.stop();
-        say.speak(text);
+    speak(text) {
+        if (settings.settings.tts.offline) {
+            say.stop();
+            say.speak(text);
+            return;
+        }
+        if (yatts ==null){
+            initya();
+        }
+        yatts.speak(text)
     }
+}
+
+function initya(){
+    yatts = window.ya.speechkit.Tts(
+        // Настройки синтеза. Список доступных настроек см. в справочнике.
+        {
+          // API-ключ. Может быть задан глобально через объект ya.speechkit.settings.
+          apikey: yaapikey,
+          speaker: 'jane'  
+        }
+      );
 }
