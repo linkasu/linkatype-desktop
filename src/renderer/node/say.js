d1a0648f1118801fceca5c028bb15a57306c2561
Ivan Bakaidov <aacidov@Air-Ivan.Dlink>
2017-12-15 18:38:06 +0300

    add stop function for wtts

--- src/renderer/node/say.js
+++ src/renderer/node/say.js
@@ -1,51 +1,62 @@
-    import say from 'say';
-    import yakit from './yandexspeechkit';
-    import wsay from './wsay'
+import say from 'say';
+import yakit from './yandexspeechkit';
+import wsay from './wsay'
 
-    import SettingsVue from '../components/LandingPage/Settings.vue'
+import SettingsVue from '../components/LandingPage/Settings.vue'
 
-    const yaapikey = 'a095c3ee-6695-4311-bae4-e875dca7e131';
+const isWin = process.platform === 'win32'
+const yaapikey = 'a095c3ee-6695-4311-bae4-e875dca7e131'
 
-    let settings = SettingsVue.data();
+let settings = SettingsVue.data();
 
-    yakit.call(window)
+yakit.call(window)
 
-    let yatts = null;
+let yatts = null;
 
-    let speaking = false;
+let speaking = false;
 
-    export default {
-        speak(text) {
-            if (settings.settings.tts.offline) {
-                if(process.platform==='win32'){
-                    wsay(text)
-                    return;
-                }
-                say.stop();
-                say.speak(text);
+export default {
+    speak(text) {
+        this.stop()
+
+        if (settings.settings.tts.offline) {
+            if (isWin) {
+                wsay.say(text)
                 return;
             }
-            if (yatts == null) {
-                initya();
+            say.speak(text);
+            return;
+        }
+        if (yatts == null) {
+            initya();
+        }
+        yatts.speak(text, {
+            speaker: settings.settings.tts.voice,
+            stopCallback() {
+
             }
-            if (!speaking) {
-                speaking = true;
-                yatts.speak(text, {
-                    speaker: settings.settings.tts.voice,
-                    stopCallback() {
-                        speaking = false;
-                    }
-                });
+        });
+    },
+    stop() {
+        if (settings.settings.tts.offline) {
+            if (isWin) {
+                wsay.stop()
+                return
             }
+            say.stop()
+            return
+        }
+        if (yatts != null) {
+            yatts.stop()
         }
     }
-
-    function initya() {
-        yatts = window.ya.speechkit.Tts(
-            // Настройки синтеза. Список доступных настроек см. в справочнике.
-            {
-                // API-ключ. Может быть задан глобально через объект ya.speechkit.settings.
-                apikey: yaapikey
-            }
-        );
-    }
+}
+function initya() {
+    yatts = window.ya.speechkit.Tts(
+        // Настройки синтеза. Список доступных настроек см. в справочнике.
+        {
+            // API-ключ. Может быть задан глобально через объект ya.speechkit.settings.
+            apikey: yaapikey
+        }
+    );
+}
--- src/renderer/node/wsay.js
+++ src/renderer/node/wsay.js
@@ -8,14 +8,20 @@ const tmp = path.join(os.tmpdir(), '/say.txt')
 
 let proc
 
-export default async (text) => {
-    try {
-        await fs.outputFile(tmp, text)
-        proc = execFile(bin, ['-v', 100, '-k', tmp], (err)=>{
-            if (err) console.log(err)
-        })
-        
-    } catch (e){
-        console.error(e)
+export default {
+    speak: async (text) => {
+        try {
+            await fs.outputFile(tmp, text)
+            proc = execFile(bin, ['-v', 100, '-k', tmp], (err) => {
+                if (err) console.log(err)
+            })
+
+        } catch (e) {
+            console.error(e)
+        }
+    },
+    stop(){
+        proc.kill(-8)
     }
+
 }
